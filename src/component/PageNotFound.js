953e8b4490cd60131a5c8f6ec3973b412a062f66
govinda1999 <gspatel61199@gmail.com>
2019-06-11 19:52:29 +0530

    added page not found component

--- src/component/PageNotFound.js
+++ src/component/PageNotFound.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react';
 export default class PageNotFound extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello from PageNotFound</h1>
+      <div className='container mt-2'>
+        <a className='btn btn-primary mb-2' href='/' role='button'>
+          Go To HOMEPAGE
+        </a>
+        <div className='text-center mb-3'>
+          <img
+            src='./img/error.jpg'
+            alt='ERROR'
+            className='img-fluid'
+            style={{ height: '70vh' }}
+          />
+        </div>
+        <h5 className='text-center text-danger'>
+          Unfortunately the page you are looking for has been moved or deleted
+        </h5>
       </div>
     );
   }
--- src/component/Product.js
+++ src/component/Product.js
@@ -4,7 +4,7 @@ export default class Product extends Component {
   render() {
     const { title, price, img_url } = this.props.product;
     return (
-      <div className='col-lg-3 col-md-4 col-sm-6 col-12  mt-2'>
+      <div className='col-lg-3 col-md-4 col-sm-6 col-12  mt-3 mb-3'>
         <img
           src={img_url}
           alt='product_image'
@@ -14,6 +14,12 @@ export default class Product extends Component {
         <p className='mt-2 text-center'>
           <span className='font-weight-bold mr-2'>{title}</span>${price}
         </p>
+        <button className='btn btn-warning btn-center text-white'>
+          <span className='text-white mr-2'>
+            <i className='fas fa-cart-plus' />
+          </span>
+          Add to cart
+        </button>
       </div>
     );
   }
